236a0bad70e6c7301cbe1aaa47e277704a7e2ac3
alexexisted <a.assjones@list.ru>
2024-05-04 13:35:43 +0300

    new dependencies for logging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
+    //okhttp
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
