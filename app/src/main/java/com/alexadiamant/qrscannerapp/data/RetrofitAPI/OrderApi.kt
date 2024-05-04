de2f0c88f5bc6dc62237fc379346b7b6926acedf
alexexisted <a.assjones@list.ru>
2024-05-04 13:38:12 +0300

    new endpoints to get ordered items

--- app/src/main/java/com/alexadiamant/qrscannerapp/data/RetrofitAPI/OrderApi.kt
+++ app/src/main/java/com/alexadiamant/qrscannerapp/data/RetrofitAPI/OrderApi.kt
@@ -1,11 +1,24 @@
 package com.alexadiamant.qrscannerapp.data.RetrofitAPI
 
+import com.alexadiamant.qrscannerapp.data.dataClasses.Items
 import com.alexadiamant.qrscannerapp.data.dataClasses.Order
+import com.alexadiamant.qrscannerapp.data.dataClasses.OrderedItems
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface OrderApi {
 //    @GET("https://api.mockfly.dev/mocks/060e9d53-0e78-4171-80cc-c4084031cad7/parcel")
+
+    //get request to get order info from json
     @GET("{endpoint}")
     suspend fun getOrder(@Path("endpoint") endpoint: String): Order
+
+    //get request to get order list of ordered items from json
+    @GET("{endpoint}")
+    suspend fun getOrderedItems(@Path("endpoint") endpoint: String): OrderedItems
+
+    //get request to get ordered item from json
+    @GET("{endpoint}")
+    suspend fun getItems(@Path("endpoint") endpoint: String): Items
+
 }
