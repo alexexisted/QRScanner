81ba85764f2b7c11807ed2467a64c71b4cdded3f
alexexisted <a.assjones@list.ru>
2024-05-04 13:39:34 +0300

    added interceptor and client to see requests and responses from server in a logcat

--- app/src/main/java/com/alexadiamant/qrscannerapp/ResultFromQRFragment.kt
+++ app/src/main/java/com/alexadiamant/qrscannerapp/ResultFromQRFragment.kt
@@ -1,20 +1,23 @@
 package com.alexadiamant.qrscannerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.alexadiamant.qrscannerapp.data.RetrofitAPI.OrderApi
+import com.alexadiamant.qrscannerapp.data.dataClasses.Items
+import com.alexadiamant.qrscannerapp.data.dataClasses.Order
 import com.alexadiamant.qrscannerapp.databinding.FragmentResultFromQrBinding
-import com.alexadiamant.qrscannerapp.logic.contracts.LinksContract
 import com.alexadiamant.qrscannerapp.logic.implementations.LinksContractImpl
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class ResultFromQRFragment : Fragment() {
 
@@ -47,18 +50,52 @@ class ResultFromQRFragment : Fragment() {
 
     private fun runRetrofit(endpoint: String) {
 
+        val TAG = "ResultFragment"
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val retrofit = Retrofit.Builder()
 //            .baseUrl("{$link}") //runtime error occurs
-            .baseUrl("https://api.mockfly.dev/mocks/060e9d53-0e78-4171-80cc-c4084031cad7/")
+            .baseUrl("https://api.mockfly.dev/mocks/060e9d53-0e78-4171-80cc-c4084031cad7/").client(client)
             .addConverterFactory(GsonConverterFactory.create()).build()
 
         val orderApi = retrofit.create(OrderApi::class.java)
 
+        //create coroutine
         CoroutineScope(Dispatchers.IO).launch {
-            val order = orderApi.getOrder(endpoint)
+            //async request to get main info about order
+            val orderDeferred: Deferred<Order> = async {
+                orderApi.getOrder(endpoint)
+            }
+            //async request to get list of ordered items
+//            val orderedItemsDeferred: Deferred<OrderedItems> = async {
+//                orderApi.getOrderedItems(endpoint)
+//            }
+//            Log.i(TAG, "$orderedItemsDeferred")
 
-            view?.post{
+            //async request to get ordered item
+            val orderedItemsDeferred: Deferred<Items> = async {
+                orderApi.getItems(endpoint)
+            }
+            Log.i(TAG, "$orderedItemsDeferred")
+
+
+            //await result
+            val order: Order = orderDeferred.await()
+//            val orderedItems: OrderedItems = orderedItemsDeferred.await()
+            val orderedItems: Items = orderedItemsDeferred.await()
+            Log.i(TAG, "$orderedItems")
+
+            //ui thread to set info from request
+            withContext(Dispatchers.Main) {
                 binding.testTV.text = order.customer.toString()
+//                binding.TVOrderedItems.text =  orderedItems.toString()
+                binding.TVOrderedItems.text = orderedItems.toString()
             }
         }
     }
